66a922e606140a7ad4e6ff6f45380c76062bce08
sunshine2web <sunshine2web@gmail.com>
2021-10-08 13:13:43 -0700

    Return [] if there are options with no text

--- app/Feeds/Vendors/TAS/Parser.php
+++ app/Feeds/Vendors/TAS/Parser.php
@@ -2,12 +2,15 @@
 
 namespace App\Feeds\Vendors\TAS;
 
+use App\Feeds\Feed\FeedItem;
 use App\Feeds\Parser\HtmlParser;
 use App\Feeds\Utils\ParserCrawler;
 use App\Helpers\StringHelper;
 
 class Parser extends HtmlParser
 {
+    private bool $extra_product = false;
+
     public function getMpn(): string
     {
         return $this->getText( '.product_code' );
@@ -32,9 +35,9 @@ class Parser extends HtmlParser
     {
         $d = trim( $this->getHtml( '[itemprop="description"]' ) ) ?: trim( $this->getHtml( '#product_description' ) );
         if ( $this->exists( '#ProductDetail_ProductDetails_div2' ) ) {
-            $d .= '<br />' . StringHelper::removeSpaces( $this->getHtml( '#ProductDetail_ProductDetails_div2 table table td' ) );
+            $d .= "<br>" . StringHelper::removeSpaces( $this->getHtml( '#ProductDetail_ProductDetails_div2 table table td' ) );
         }
-        return StringHelper::isNotEmpty($d) ? trim($d) : $this->getProduct();
+        return StringHelper::isNotEmpty( $d ) ? trim( $d ) : $this->getProduct();
     }
 
     public function getImages(): array
@@ -44,55 +47,71 @@ class Parser extends HtmlParser
 
     public function getAvail(): ?int
     {
-        preg_match("/Quantity.in.Stock:(\d+)</im", $this->node->html(), $matches);
-        if ( !isset($matches[1]) ) {
+        preg_match( "/Quantity.in.Stock:(\d+)</im", $this->node->html(), $matches );
+        if ( !isset( $matches[ 1 ] ) ) {
             return stripos( $this->getAttr( 'meta[ itemprop="availability"]', 'content' ), 'InStock' ) ? self::DEFAULT_AVAIL_NUMBER : 0;
         }
-        return $matches[1] ?? self::DEFAULT_AVAIL_NUMBER;
+        return $matches[ 1 ] ?? self::DEFAULT_AVAIL_NUMBER;
     }
 
     public function getAttributes(): ?array
     {
         $attributes = [];
-        if (! $this->exists( '[itemprop="offers"]' )) {
+        if ( !$this->exists( '[itemprop="offers"]' ) ) {
             return null;
         }
         $offers = $this->filter( '[itemprop="offers"]' )->html();
-        $valid_names = ['Made In', 'Fabric', 'Color', 'Size Ratio', 'Package']; 
+        $valid_names = [ 'Made In', 'Fabric', 'Color', 'Size Ratio', 'Package' ];
 
-        foreach (explode( '<br>', $offers ) as $o) {
-            $item = trim($o);
-            preg_match("/<b>(.*?):<\/b>(.*)/im", $item, $matches);
-            if (isset($matches[2])) {
-                $name = trim($matches[1]);
-                $v = trim($matches[2]);
-                if (in_array($name, $valid_names)) {
+        foreach ( explode( '<br>', $offers ) as $o ) {
+            $item = trim( $o );
+            preg_match( "/<b>(.*?):<\/b>(.*)/im", $item, $matches );
+            if ( isset( $matches[ 2 ] ) ) {
+                $name = trim( $matches[ 1 ] );
+                $v = trim( $matches[ 2 ] );
+                if ( in_array( $name, $valid_names ) ) {
                     $attributes[ $name ] = $v;
                 }
             }
         }
-        
-        return empty($attributes) ? null : $attributes;
+
+        return empty( $attributes ) ? null : $attributes;
+    }
+
+    public function afterParse( FeedItem $fi ): void
+    {
+        if ( $this->extra_product ) {
+            $fi->setCostToUs( 0 );
+        }
     }
 
     public function getOptions(): array
     {
+        $find_empty_option = false;
         $options = [];
-        
-        $this->filter( '#options_table select' )->each( function ( ParserCrawler $node ) use ( &$options ) {
+
+        $this->filter( '#options_table select' )->each( function ( ParserCrawler $node ) use ( &$options, &$find_empty_option ) {
             $name_option = '';
-            foreach( $node->getContent( 'option' ) as $option ) {
+            foreach ( $node->getContent( 'option' ) as $option ) {
                 if ( stripos( $option, 'select' ) !== false ) {
                     $name_option = StringHelper::normalizeSpaceInString( str_ireplace( 'select', '', $option ) );
                     break;
                 }
             }
-            preg_match('/makeComboGroup\("' . $node->attr( 'name' )  .  '.*?TCN_reload/s', $this->node->html(), $matches);
-            preg_match_all('/TCN_addContent\(\"(.*?)\+/i', $matches[0], $matches);
-            if (!empty($matches[1])) {
-                $options[ $name_option ] = $matches[1];
+            preg_match( '/makeComboGroup\("' . $node->attr( 'name' ) . '.*?TCN_reload/s', $this->node->html(), $matches );
+            preg_match_all( '/TCN_addContent\(\"(.*?)\+/i', $matches[ 0 ], $matches );
+            if ( !empty( $matches[ 1 ] ) ) {
+                $options[ $name_option ] = $matches[ 1 ];
             }
-        });
+            else {
+                $find_empty_option = true;
+            }
+        } );
+
+        if ( $find_empty_option && $this->exists( '#options_table select' ) ) {
+            $this->extra_product = true;
+        }
+
         return $options;
     }
 }
